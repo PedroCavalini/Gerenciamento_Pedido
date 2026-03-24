eac2f3e31c40c1c5d82028d38d40251b13ab9d04
PedroCavalini <pedrogameleira01@gmail.com>
2026-03-24 15:12:40 -0300

    Incluindo PedidoService

--- src/main/java/com/example/Pedidos/service/PedidoService.java
+++ src/main/java/com/example/Pedidos/service/PedidoService.java
@@ -1,4 +1,33 @@
 package com.example.Pedidos.service;
 
+import com.example.Pedidos.model.PedidoModel;
+import com.example.Pedidos.repository.PedidoRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class PedidoService {
+
+    private final PedidoRepository repository;
+
+    public PedidoService(PedidoRepository repository) {
+        this.repository = repository;
+    }
+
+    public PedidoModel salvar(PedidoModel pedido) {
+        return repository.save(pedido);
+    }
+
+    public List<PedidoModel> listarTodos() {
+        return repository.findAll();
+    }
+
+    public Optional<PedidoModel> buscarPorId(Long id) {
+        return repository.findById(id);
+    }
+
+    public void deletar(Long id) {
+        repository.deleteById(id);
+    }
 }
