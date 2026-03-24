9bb56f03f7ae32152216f48bd1e391f50a13a2e4
PedroCavalini <pedrogameleira01@gmail.com>
2026-03-24 15:11:10 -0300

    Incluindo PedidoRepository

--- src/main/java/com/example/Pedidos/repository/PedidoRepository.java
+++ src/main/java/com/example/Pedidos/repository/PedidoRepository.java
@@ -1,4 +1,9 @@
 package com.example.Pedidos.repository;
 
-public interface PedidoRepository {
+import com.example.Pedidos.model.PedidoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<PedidoModel, Long> {
 }
